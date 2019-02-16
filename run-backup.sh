bb239b4607e4401a0b1641d6dbf68d4efda44fc7
Antti Kivimäki <antti.kivimaki@futurice.com>
2019-02-16 18:07:05 +0200

    Check if pg_dump was successful before uploading

--- run-backup.sh
+++ run-backup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 FILE_NAME=${PGHOSTNAME}_dump_`date "+%Y-%m-%dT%H-%M-%S"`.sql.gz
+FILE_PATH=/root/dumps/$FILE_NAME
 
-pg_dump ${PGDATABASE:-postgres} -c -h $PGHOSTNAME -U ${PGUSERNAME:-postgres} | gzip > /root/dumps/$FILE_NAME
-/root/dropbox_uploader.sh upload /root/dumps/$FILE_NAME $FILE_NAME
+pg_dump ${PGDATABASE:-postgres} -c -h $PGHOSTNAME -U ${PGUSERNAME:-postgres} | gzip > $FILE_PATH
+if [ $? -eq 0 ]; then
+  /root/dropbox_uploader.sh upload $FILE_PATH $FILE_NAME
+  rm $FILE_PATH
+fi
