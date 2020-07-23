b6b151ccf0eb5b18ad869fe124c55166c439a363
Antti Kivimäki <antti.kivimaki@futurice.com>
2020-07-23 17:19:03 +0300

    Refactor script a bit

--- run-backup.sh
+++ run-backup.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-export PGPASSWORD=$PG_PASSWORD # pg_dump reads password from variable PGPASSWORD
-
 PG_DATABASE=${PG_DATABASE:-postgres}
+PG_USERNAME=${PG_USERNAME:-postgres}
+
+WORKING_DIR=/root/dumps
 FILE_NAME=${PG_HOSTNAME}_${PG_DATABASE}_dump_`date "+%Y-%m-%dT%H-%M-%S"`.sql.gz
 
-pg_dump ${PG_DATABASE} -c -h $PG_HOSTNAME -U ${PG_USERNAME:-postgres} | gzip > /root/dumps/$FILE_NAME
+export PGPASSWORD=$PG_PASSWORD # pg_dump reads password from variable PGPASSWORD
+
+pg_dump ${PG_DATABASE} -w -c -h $PG_HOSTNAME -U ${PG_USERNAME} | gzip > $WORKING_DIR/$FILE_NAME
 if [ -n $PASSWORD ]; then
-  openssl enc -aes-256-cbc -pass env:PASSWORD -in /root/dumps/$FILE_NAME -out /root/dumps/$FILE_NAME.enc
-  rm /root/dumps/$FILE_NAME
+  openssl enc -aes-256-cbc -pass env:PASSWORD -in $WORKING_DIR/$FILE_NAME -out $WORKING_DIR/$FILE_NAME.enc
+  rm $WORKING_DIR/$FILE_NAME
   FILE_NAME=$FILE_NAME.enc
 fi
 
 if [ $? -eq 0 ]; then
-  /root/dropbox_uploader.sh upload /root/dumps/$FILE_NAME $FILE_NAME
-  rm /root/dumps/$FILE_NAME
+  /root/dropbox_uploader.sh upload $WORKING_DIR/$FILE_NAME $FILE_NAME
+  rm $WORKING_DIR/$FILE_NAME
 fi
